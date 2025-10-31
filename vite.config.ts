d260e2db778f7cc20fc8f9926c90ca8b1c39b82a
MiraProfissional <jvitor.gmira@gmail.com>
2025-10-31 14:09:19 -0300

    :recycle: feat: adapting system to integrated with the backend by Docker

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      'all',
+      '192.168.1.108',
+      'localhost',
+      '127.0.0.1',
+    ],
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: false,
+  },
 })
